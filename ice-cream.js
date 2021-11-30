eb67b15aa45717c9c076e92b8b3e3e6c9ab6d686
John Ming <john.ngai@hotmail.com>
2021-11-30 00:24:15 +0000

    Correct solution for the Ice Cream assignment

---
+++ ice-cream.js
@@ -0,0 +1,13 @@
+// Do not modify these first two lines
+const iceCreamFlavours = ["chocolate", "vanilla", "cookies and cream", "rocky road", "strawberry"];
+console.log(iceCreamFlavours);
+
+// Your code below here...
+iceCreamFlavours.push('root beer'); // Add the flavour "root beer" to the array without modifying the original array.
+console.log(iceCreamFlavours); // Logged the value of the entire array.
+
+console.log(iceCreamFlavours[0]); // Logged the first flavour in the array.
+
+console.log(iceCreamFlavours[iceCreamFlavours.length - 1]); // Logged the last flavour in the array.
+
+console.log(iceCreamFlavours.length); // Logged the total number of flavours in the array.
